941b4fb73c8057490acc3b563c21ce192447d5f9
Supachai Yuenyao <flookes2541@gmail.com>
2024-09-30 10:18:41 +0700

    update customer section

--- src/views/landingPage/components/Customer.js
+++ src/views/landingPage/components/Customer.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import '../css/customer.css';
 
 const Customer = () => {
-  return <div>Customer</div>;
+  return (
+    <>
+      <div className="customer d-none d-lg-flex">
+        <img src="/img/logo/customer1.png" alt="" />
+        <img src="/img/logo/customer2.png" alt="" />
+        <img src="/img/logo/customer3.png" alt="" />
+        <img src="/img/logo/customer4.png" alt="" />
+        <img src="/img/logo/customer5.png" alt="" />
+      </div>
+      <div className="customer-mobile d-flex d-lg-none">
+        <img src="/img/logo/customer-mobile-1.jpeg" alt="" />
+        <img src="/img/logo/customer-mobile-2.jpeg" alt="" />
+        <img src="/img/logo/customer-mobile-3.jpeg" alt="" />
+        <img src="/img/logo/customer-mobile-4.jpeg" alt="" />
+        <img src="/img/logo/customer-mobile-5.jpeg" alt="" />
+        <img src="/img/logo/customer-mobile-6.jpeg" alt="" />
+        <img src="/img/logo/customer-mobile-7.jpeg" alt="" />
+        <img src="/img/logo/customer-mobile-8.jpeg" alt="" />
+        <img src="/img/logo/customer-mobile-9.jpeg" alt="" />
+        <img src="/img/logo/customer-mobile-10.jpeg" alt="" />
+        <img src="/img/logo/customer-mobile-11.jpeg" alt="" />
+      </div>
+    </>
+  );
 };
 
 export default Customer;
